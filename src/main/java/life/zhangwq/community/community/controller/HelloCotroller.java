d542fb6cdfd0ec9401b36d6834e71ca6435197da
zhangwq1112 <893102122@qq.com>
2019-10-12 11:38:26 +0800

    init repo

---
+++ src/main/java/life/zhangwq/community/community/controller/HelloCotroller.java
@@ -0,0 +1,19 @@
+package life.zhangwq.community.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Created by zhangwq on 2019/10/12.
+ */
+@Controller
+public class HelloCotroller {
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name="name", required=false, defaultValue = "world") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello";
+    }
+}
